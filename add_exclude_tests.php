78bcf43d6fbba1d3c821c03569fe52cf0d9476c0
Adrien Gallou <adriengallou@gmail.com>
2014-07-12 21:11:09 +0200

    classement des tests exclus

--- add_exclude_tests.php
+++ add_exclude_tests.php
@@ -2,6 +2,8 @@
 
 $filePath = $argv[1];
 
+$excludeSkippedAndIncomplete = true;
+
 if (!is_file($filePath)) {
     throw new \InvalidArgumentException(sprintf('File %s does not exists', $filePath));
 }
@@ -18,8 +20,30 @@ if (1 !== $testSuiteSearch->length) {
 
 $testSuiteNode = $testSuiteSearch->item(0);
 
-$excludedFiles = array(
+$failingTests = array(
+    //Test non à jour ?
+    './src/Symfony/Component/Intl/Tests/ResourceBundle/Reader/BinaryBundleReaderTest.php',
+    './src/Symfony/Component/Intl/Tests/DateFormatter/AbstractIntlDateFormatterTest.php',
+    './src/Symfony/Component/Intl/Tests/DateFormatter/IntlDateFormatterTest.php',
+    './src/Symfony/Component/Intl/Tests/DateFormatter/Verification/IntlDateFormatterTest.php',
+
+    //Sigchild environnment ?
+    './src/Symfony/Component/Process/Tests/SigchildDisabledProcessTest.php',
+    './src/Symfony/Component/Process/Tests/SigchildEnabledProcessTest.php',
+
+    //proc_open(/dev/tty): failed to open stream: No such device or address
+    './src/Symfony/Component/Process/Tests/SimpleProcessTest.php',
+);
+
+$excludedTests = array(
 // skipped test
+
+    // pourquoi le test remonte qu'on est en user root ?
+    './src/Symfony/Component/HttpFoundation/Tests/File/MimeType/MimeTypeTest.php',
+
+    //Pas de constante PHP_BINARY alors qu'on est en 5.5 et qu'elle se trouve à partir de 5.4 ?
+    './src/Symfony/Component/Process/Tests/PhpExecutableFinderTest.php',
+
     //Le test semble inutile (test toujours skipped)
     './src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListCompositeIdTest.php',
     './src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListCompositeIdWithQueryBuilderTest.php',
@@ -27,50 +51,6 @@ $excludedFiles = array(
     './src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleIntIdWithQueryBuilderTest.php',
     './src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleStringIdTest.php',
     './src/Symfony/Bridge/Doctrine/Tests/Form/ChoiceList/UnloadedEntityChoiceListSingleStringIdWithQueryBuilderTest.php',
-
-    //nécéssite APC et on utilise php 5.5
-    './src/Symfony/Component/ClassLoader/Tests/ApcUniversalClassLoaderTest.php',
-    './src/Symfony/Component/Validator/Tests/Mapping/Cache/ApcCacheTest.php',
-    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/FrameworkExtensionTest.php',
-    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/PhpFrameworkExtensionTest.php',
-    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/YamlFrameworkExtensionTest.php',
-    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/XmlFrameworkExtensionTest.php',
-
-    //Test non à jour ?
-    './src/Symfony/Component/Intl/Tests/ResourceBundle/Reader/BinaryBundleReaderTest.php',
-    './src/Symfony/Component/Intl/Tests/DateFormatter/AbstractIntlDateFormatterTest.php',
-    './src/Symfony/Component/Intl/Tests/DateFormatter/IntlDateFormatterTest.php',
-    './src/Symfony/Component/Intl/Tests/DateFormatter/Verification/IntlDateFormatterTest.php',
-
-    //Contient un test pour windows (système de fichier case insensitive, créer un groupe ?)
-    './src/Symfony/Component/Debug/Tests/DebugClassLoaderTest.php',
-    './src/Symfony/Component/Debug/Tests/FatalErrorHandler/ClassNotFoundFatalErrorHandlerTest.php',
-
-    //This test is for Windows platform only
-    './src/Symfony/Component/Process/Tests/SimpleProcessTest.php',
-
-    //test 32 bits (créer groupe ?)
-    './src/Symfony/Component/Form/Tests/Extension/Core/Type/DateTypeTest.php',
-    './src/Symfony/Component/Intl/Tests/NumberFormatter/NumberFormatterTest.php',
-    './src/Symfony/Component/Intl/Tests/NumberFormatter/Verification/NumberFormatterTest.php',
-
-    // pourquoi le test remonte qu'on est en user root ?
-    './src/Symfony/Component/HttpFoundation/Tests/File/MimeType/MimeTypeTest.php',
-
-    //contient deux  cas distints : PHP compilé avec option disable-ipv6 et sans
-    './src/Symfony/Component/HttpFoundation/Tests/IpUtilsTest.php',
-
-    //Contient un test spécifique à php 5.3
-    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/NativeSessionStorageTest.php',
-    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/PhpBridgeSessionStorageTest.php',
-    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Proxy/AbstractProxyTest.php',
-
-    //Pas de constante PHP_BINARY alors qu'on est en 5.5 et qu'elle se trouve à partir de 5.4 ?
-    './src/Symfony/Component/Process/Tests/PhpExecutableFinderTest.php',
-
-    //Sigchild environnment ?
-    './src/Symfony/Component/Process/Tests/SigchildDisabledProcessTest.php',
-    './src/Symfony/Component/Process/Tests/SigchildEnabledProcessTest.php',
 
 
 //Incomplete tests
@@ -80,11 +60,61 @@ $excludedFiles = array(
 
     //The Yaml Dumper currently does not support prototyped arrays
     './src/Symfony/Component/Config/Tests/Definition/Dumper/YamlReferenceDumperTest.php',
+
 );
 
 
-foreach ($excludedFiles as $excludedFile) {
+$groups = array(
+  // APC alors que l'on se trouve en version 5.5
+  'apc' => array(
+    './src/Symfony/Component/ClassLoader/Tests/ApcUniversalClassLoaderTest.php',
+    './src/Symfony/Component/Validator/Tests/Mapping/Cache/ApcCacheTest.php',
+    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/FrameworkExtensionTest.php',
+    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/PhpFrameworkExtensionTest.php',
+    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/YamlFrameworkExtensionTest.php',
+    './src/Symfony/Bundle/FrameworkBundle/Tests/DependencyInjection/XmlFrameworkExtensionTest.php',
+  ),
+  'ci_fs' => array(
+    //Contient un test pour windows (système de fichier case insensitive, créer un groupe ?)
+    './src/Symfony/Component/Debug/Tests/DebugClassLoaderTest.php',
+    './src/Symfony/Component/Debug/Tests/FatalErrorHandler/ClassNotFoundFatalErrorHandlerTest.php',
+  ),
+  'windows' => array(
+    './src/Symfony/Component/Process/Tests/SimpleProcessTest.php',
+  ),
+  '32bits' => array(
+    './src/Symfony/Component/Form/Tests/Extension/Core/Type/DateTypeTest.php',
+    './src/Symfony/Component/Intl/Tests/NumberFormatter/NumberFormatterTest.php',
+    './src/Symfony/Component/Intl/Tests/NumberFormatter/Verification/NumberFormatterTest.php',
+  ),
+  'ipv6' => array(
+    //contient deux  cas distints : PHP compilé avec option disable-ipv6 et sans
+    './src/Symfony/Component/HttpFoundation/Tests/IpUtilsTest.php',
+  ),
+  'php53' => array(
+    //Contient un test spécifique à php 5.3
+    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/NativeSessionStorageTest.php',
+    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/PhpBridgeSessionStorageTest.php',
+    './src/Symfony/Component/HttpFoundation/Tests/Session/Storage/Proxy/AbstractProxyTest.php',
+  ),
+);
+
+
+foreach ($failingTests as $excludedFile) {
     $testSuiteNode->appendChild($dom->createElement('exclude', $excludedFile));
 }
+
+if ($excludeSkippedAndIncomplete) {
+  foreach ($excludedTests as $excludedFile) {
+      $testSuiteNode->appendChild($dom->createElement('exclude', $excludedFile));
+  }
+
+  foreach ($groups as $name => $files) {
+    foreach ($files as $file) {
+      $testSuiteNode->appendChild($dom->createElement('exclude', $file));
+    }
+  }
+}
+
 
 $dom->save($filePath);
